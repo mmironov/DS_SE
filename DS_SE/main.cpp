b4487080198ab8c34ef7b19d39e842f16ae37ee6
Miroslav <miroslavmironov@gmail.com>
2015-07-05 15:45:18 +0300

    Initial Commit

---
+++ DS_SE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DS_SE
+//
+//  Created by Miroslav Mironov on 7/5/15.
+//  Copyright (c) 2015 Miroslav Mironov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
